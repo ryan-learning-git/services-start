9dd90a5034ed9b347c6bd8158b272d401c89207c
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-23 15:21:32 +0000

    Completed 102

--- src/app/Shared/accounts.service.ts
+++ src/app/Shared/accounts.service.ts
@@ -1,5 +1,5 @@
 import {LoggingService} from './logging.service';
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 @Injectable()
 export class AccountService {
